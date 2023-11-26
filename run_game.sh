d007dfbb9350743d128a0e701b0db58d2b5ec161
your name <your email>
2023-11-26 18:05:39 +0100

    Created a script that runt my java files automatic

---
+++ run_game.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+GREATING="Welcome $USER"
+
+echo "$GREATING"
+
+name="Ludwig_Wagberg_labb"
+
+mkdir "$name"
+
+cp /home/ludwag/guessingGame/Guesser.java /home/ludwag/guessingGame/Ludwig_Wagberg_labb
+cp /home/ludwag/guessingGame/GuessingGame.java /home/ludwag/guessingGame/Ludwig_Wagberg_labb
+cp /home/ludwag/guessingGame/run_game.sh /home/ludwag/guessingGame/Ludwig_Wagberg_labb
+
+cd /home/ludwag/guessingGame/Ludwig_Wagberg_labb
+echo "Running game from "
+pwd
+
+
+echo "Compiling..."
+javac GuessingGame.java
+javac Guesser.java
+
+echo "Running..."
+java GuessingGame
+
+echo "Done!"
+
+ls
+
+rm -r /home/ludwag/guessingGame/Ludwig_Wagberg_labb
