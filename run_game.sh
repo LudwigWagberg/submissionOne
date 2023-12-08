d9e12ce95b728c56f8c06593ecd607464fc321b6
your name <your email>
2023-12-08 11:34:19 +0100

    Fixing cd and cp in run_game.sh

--- run_game.sh
+++ run_game.sh
@@ -8,11 +8,10 @@ name="Ludwig_Wagberg_labb"
 
 mkdir "$name"
 
-cp /home/ludwag/guessingGame/Guesser.java /home/ludwag/guessingGame/Ludwig_Wagberg_labb
-cp /home/ludwag/guessingGame/GuessingGame.java /home/ludwag/guessingGame/Ludwig_Wagberg_labb
-cp /home/ludwag/guessingGame/run_game.sh /home/ludwag/guessingGame/Ludwig_Wagberg_labb
+cp Guesser.java Ludwig_Wagberg_labb
+cp GuessingGame.java Ludwig_Wagberg_labb
 
-cd /home/ludwag/guessingGame/Ludwig_Wagberg_labb
+cd Ludwig_Wagberg_labb
 echo "Running game from "
 pwd
 
